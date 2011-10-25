1d6d04846ad58c5b305c755b4f78ab04a91149de
Chris Ege <chrisege@gmail.com>
2011-10-24 23:43:53 -0500

    User signup complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,15 +27,9 @@ class User < ActiveRecord::Base
   
   def self.authenticate(email, submitted_password)
     user = find_by_email(email)
-    return nil if user.nil?
-    return user if user.has_password?(submitted_password)
+    user && user.has_password?(submitted_password) ? user : nil
   end
-  
-  #def User.authenticate(email, submitted_password)
-  #  user = find_by_email(email)
-  #  return nil if user.nil?
-  #  return user if user.has_password?(submitted_password)
-  #end
+
   
   private
 
